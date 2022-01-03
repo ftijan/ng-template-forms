ef063795c17be23580ec6f60597b06dfd78be227
Filip Tijan <ftijan@gmail.com>
2022-01-03 01:26:50 +0000

    form submit validation, css, onblur validation

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -24,4 +25,11 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
 }
