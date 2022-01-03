f9bf3b9291a6e91682426a028c8635cf4a28e3f3
Filip Tijan <ftijan@gmail.com>
2022-01-03 04:02:47 +0000

    http requests and error handling

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { DataService } from '../data/data.service';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -7,7 +9,7 @@ import { UserSettings } from '../data/user-settings';
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent implements OnInit {
+export class UserSettingsFormComponent implements OnInit, OnDestroy {
   originalUserSettings: UserSettings = {
     name: 'Milton',
     emailOffers: true,
@@ -20,16 +22,42 @@ export class UserSettingsFormComponent implements OnInit {
   // Would need deep cloning on highly nested data hierarchy:
   userSettings: UserSettings = { ...this.originalUserSettings };
 
-  constructor() { }
+  sub: Subscription | undefined;
+  public postError = false;
+  public postErrorMessage = '';
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
   }
 
   onSubmit(form: NgForm) {
     console.log('in onSubmit: ', form.valid);
+
+    this.postError = false;
+    this.postErrorMessage = '';
+
+    if(form.valid) {
+      this.sub = this.dataService.postUserSettingsForm(this.userSettings).subscribe({
+        next: userSettings => console.log('success: ', userSettings),
+        error: errorResponse => this.onHttpError(errorResponse)     
+      });
+    } else {
+      this.postError = true;
+      this.postErrorMessage = 'Please fix above errors';
+    }    
   }
 
   onBlur(field: NgModel) {
     console.log('in onBlur: ', field.valid);
+  }
+
+  onHttpError(errorResponse: any) {    
+    console.error('error: ', errorResponse);    
+    this.postError = true;
+    this.postErrorMessage = errorResponse.message;
   }
 }
